9256c66d24457854ad7143f7b5cf3b5c042c0257
Antonio_Not <ahssantos1@live.com>
2014-02-08 22:33:53 -0200

    Criacao do banco de dados

---
+++ dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[pkEmpresa] INT NULL PRIMARY KEY, 
+    [RazaoSocial] NCHAR(50) NULL, 
+    [NomeFantasia] NCHAR(50) NOT NULL, 
+    [CNPJ] NCHAR(50) NULL, 
+    [Endereco] NCHAR(50) NULL, 
+    [Cidade] NCHAR(50) NULL, 
+    [Estado] NCHAR(2) NULL, 
+    [Telefone] NCHAR(15) NULL
+)
